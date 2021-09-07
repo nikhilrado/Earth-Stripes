e5f1c6ce6c49af37cb056109d244edbdcad0e8f7
Nikhil R <60348432+nikhilrado@users.noreply.github.com>
2021-09-07 01:28:08 -0400

    test commit

--- test9requests.py
+++ test9requests.py
@@ -3,3 +3,4 @@ import requests
 my_headers = {'token' : 'vuqgjmWUyGoUzZHEhWswbwzdOMyvnbFk'}
 response = requests.get('https://www.ncdc.noaa.gov/cdo-web/api/v2/datacategories/ANNTEMP', headers=my_headers)
 print(response.json())
+#test commit
