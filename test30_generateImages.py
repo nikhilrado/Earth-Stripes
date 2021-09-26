74bee6f1d41172b2180e70c0b03c2d2379219ac8
Nikhil <60348432+nikhilrado@users.noreply.github.com>
2021-09-25 20:57:15 -0400

    added state/province images to be created

--- test30_generateImages.py
+++ test30_generateImages.py
@@ -1,15 +1,20 @@
 import test13_create_county_images
 import test25_create_state_images
 import test18_create_country_images
+import test42_create_province_state_images
 
 test13_create_county_images.main("stripes")
 test25_create_state_images.main("stripes")
 test18_create_country_images.main("stripes")
+test42_create_province_state_images.main("stripes")
 
 test13_create_county_images.main("labeled-bars")
 test25_create_state_images.main("labeled-bars")
 test18_create_country_images.main("labeled-bars")
+test42_create_province_state_images.main("labeled-bars")
 
 test13_create_county_images.main("labeled-stripes")
 test25_create_state_images.main("labeled-stripes")
 test18_create_country_images.main("labeled-stripes")
+test42_create_province_state_images.main("labeled-stripes")
+
