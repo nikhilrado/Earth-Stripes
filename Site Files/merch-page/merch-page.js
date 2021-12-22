fce22999aa28e214905b5301646f72f68a7d4d44
Nikhil <60348432+nikhilrado@users.noreply.github.com>
2021-12-22 16:35:38 -0500

    Merch Page Revamp -added location toggle for better ux -added zazzle logo inline on mobile -added country geolocate via browser lang CU-1v854b1

--- Site Files/merch-page/merch-page.js
+++ Site Files/merch-page/merch-page.js
@@ -1,5 +1,19 @@
 //document.getElementById("autocomplete").focus();
 
+var changeLocationBoxIsHidden = true;
+function toggleChangeLocation(action=null){
+    var element = document.getElementById("changeLocationBox");
+    if(changeLocationBoxIsHidden && action!="hide"){
+        element.style.display = "block";
+        changeLocationBoxIsHidden = false;
+    }
+    else{
+        element.style.display = "none";
+        changeLocationBoxIsHidden = true;
+    }
+    console.log(changeLocationBoxIsHidden)
+}
+
 function generateURL(countryCode = null, stateCode = null, countyName = null) {
     locationAutomaticURL = "/result/?country=" + countryCode;
     if (stateCode != false && stateCode != null) {
@@ -26,39 +40,55 @@ function generateImageID (countryCode=null, stateCode=null, countyName=null){
     return ImageID;
 }
 
+function getCountryFromCountryCode(countryCode){
+    const regionNamesInEnglish = new Intl.DisplayNames(['en'], {type:'region'});
+    return regionNamesInEnglish.of(countryCode);
+}
+
 bucketPrefix = "https://earthstripes.s3.us-east-2.amazonaws.com/v3/";
 const productNames = ["Cloth Mask","Mug","Tie","Stickers", "Magnet","Badge (Pin)", "T-Shirt", "Socks"];
 const productIDs = ['256670743725195335','168540946485519042','151119561160107608','217917661479101292','147753984968275362', '145354997245092652', '235484093576644423','256547457460896288'];
 function setMerchBox(imageID, locationName, locationURL){
-var encodedLabeledStripesImageURL = encodeURI(bucketPrefix + "labeled-stripes/" + imageID + ".png?request=zazzle");
-console.log(bucketPrefix + "labeled-stripes/" + imageID + ".png?request=zazzle");
-var encodedStripesImageURL = encodeURI(bucketPrefix + "stripes/" + imageID + ".png?request=zazzle");
+    var encodedLabeledStripesImageURL = encodeURI(bucketPrefix + "labeled-stripes/" + imageID + ".png?request=zazzle");
+    console.log(bucketPrefix + "labeled-stripes/" + imageID + ".png?request=zazzle");
+    var encodedStripesImageURL = encodeURI(bucketPrefix + "stripes/" + imageID + ".png?request=zazzle");
+    
+    merchLabel = "&t_location_txt=" + encodeURIComponent(locationName);// + " " + startYear + "-" + endYear);
+    customMerchLink = "https://www.zazzle.com/api/create/at-238391408801122257?rf=238391408801122257&ax=DesignBlast&sr=250403062909979961&cg=196064354850369877&t__useQpc=false&t__smart=false&t_labeledstripes_iid="
+    + encodedLabeledStripesImageURL + "&tc=merch-page&ic=" + imageID.replace(/[^a-zA-z]/g,'_') + "&t_stripes_iid=" + encodedStripesImageURL + merchLabel;
+    //testmerchlink.href = customMerchLink;
+    MerchButton.href = customMerchLink;
+    console.log(imageID.replace(/[^a-zA-z]/g,'_'));
+    
+    for (i=1; i<=productNames.length; i++){
+        element = document.getElementById('ProductImage' + i);
+        var imageID2 = 'https://rlv.zazzle.com/svc/view?pid='+productIDs[i-1]+'&max_dim=600&at=238391408801122257&t_stripes_url='+encodedStripesImageURL + '&t_labeledstripes_url='+encodedLabeledStripesImageURL;
+        element.src = imageID2;
+        element.alt = "Warming Stripes " + productNames[i-1];
+        element = document.getElementById('ProductName' + i);
+        element.innerText = productNames[i-1];
+        element = document.getElementById('Product'+i+"Link");
+        element.href = customMerchLink;
+        element = document.getElementById('Product'+i+'Link2');
+        element.href = customMerchLink;
+    }
+    
+    element = document.getElementById('locationName');
+    element.innerText = imageID.replaceAll("+"," ");
+    element = document.getElementById('locationNameLink');
+    console.log(locationAutomaticURL);
+    element.href = locationAutomaticURL;
+    
 
-merchLabel = "&t_location_txt=" + encodeURIComponent(locationName);// + " " + startYear + "-" + endYear);
-customMerchLink = "https://www.zazzle.com/api/create/at-238391408801122257?rf=238391408801122257&ax=DesignBlast&sr=250403062909979961&cg=196064354850369877&t__useQpc=false&t__smart=false&t_labeledstripes_iid="
-+ encodedLabeledStripesImageURL + "&tc=results-merch-box&ic=" + imageID.replace(/[^a-zA-z]/g,'_') + "&t_stripes_iid=" + encodedStripesImageURL + merchLabel;
-//testmerchlink.href = customMerchLink;
-MerchButton.href = customMerchLink;
-console.log(imageID.replace(/[^a-zA-z]/g,'_'));
-
-for (i=1; i<=productNames.length; i++){
-    element = document.getElementById('ProductImage' + i);
-    var imageID2 = 'https://rlv.zazzle.com/svc/view?pid='+productIDs[i-1]+'&max_dim=600&at=238391408801122257&t_stripes_url='+encodedStripesImageURL + '&t_labeledstripes_url='+encodedLabeledStripesImageURL;
-    element.src = imageID2;
-    element.alt = "Warming Stripes " + productNames[i-1];
-    element = document.getElementById('ProductName' + i);
-    element.innerText = productNames[i-1];
-    element = document.getElementById('Product'+i+"Link");
-    element.href = customMerchLink;
-    element = document.getElementById('Product'+i+'Link2');
-    element.href = customMerchLink;
-}
-
-element = document.getElementById('locationName');
-element.innerText = imageID.replaceAll("+"," ");
-element = document.getElementById('locationNameLink');
-console.log(locationAutomaticURL);
-element.href = locationAutomaticURL;
+    var yeet = document.getElementById("underline-text");
+    myArray= imageID.split("/");
+    locationName = myArray[myArray.length - 1].replaceAll("+"," ")
+    countryCode = myArray[0]
+    if (countryCode == locationName && locationName.length == 2){ //if it is just a country code, replace with name
+        locationName = getCountryFromCountryCode(locationName);
+    }
+    yeet.textContent = locationName;
+    toggleChangeLocation(action="hide");
 } 
 
 //custom function to get Redirect URL
@@ -108,16 +138,7 @@ function getRedirectURLMapbox(jsonPlaceData,redirectNow=false) {
         }
     }
 
-    //get lowest location name which will be used as the label for the location btn
-    lowestLocationName = countryName
-    //tried to insert this stuff next to if statement to run when country is just US
-    if (countryCode != "US"){
-        lowestLocationName = countyName //sets it to country name if not US
-    }
-    else{
-        if (stateCode != false){lowestLocationName=stateName}
-        if (countyName != false){lowestLocationName=countyName}
-    }
+    lowestLocationName = getLowestLocationName(countryCode,stateCode,countyName);
 
     generateURL(countryCode,stateCode,countyName);
     setMerchBox(generateImageID(countryCode,stateCode,countyName));
@@ -127,6 +148,20 @@ function getRedirectURLMapbox(jsonPlaceData,redirectNow=false) {
 
     x.innerHTML = lowestLocationName
     return locationAutomaticURL; 
+}
+
+function getLowestLocationName(countryCode,stateCode,countyName){
+    //get lowest location name which will be used as the label for the location btn
+    lowestLocationName = countryName
+    //tried to insert this stuff next to if statement to run when country is just US
+    if (countryCode != "US"){
+        lowestLocationName = countryName //sets it to country name if not US
+    }
+    else{
+        if (stateCode != false){lowestLocationName=stateName}
+        if (countyName != false){lowestLocationName=countyName}
+    }
+    return lowestLocationName;
 }
 
 //geolocation test stuff
@@ -227,4 +262,8 @@ google.maps.event.addListener(autocomplete, 'place_changed', function(){
     setMerchBox(imageID, countryCode, generateURL(countryCode,stateCode,countyName));
 }) 
 
-setMerchBox("US", "United States"); 
+setMerchBox("US", "United States");  
+const lang = navigator.language;
+countryLang = lang.substr(lang.length-2).toUpperCase();
+console.log(countryLang);
+setMerchBox(countryLang,countryLang,generateURL(countryLang,null,null));
