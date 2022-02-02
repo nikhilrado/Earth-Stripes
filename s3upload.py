4967dac44e975164cae3860d05a8e414bb80ba37
Nikhil <60348432+nikhilrado@users.noreply.github.com>
2022-02-01 19:05:20 -0500

    added method upload all files for one data source

--- s3upload.py
+++ s3upload.py
@@ -149,11 +149,21 @@ def test():
     #uploadNewChanges(directory="results/json/US/",smartUpload=False)
     #uploadNewChanges(directory="results/",smartUpload=False)
     #uploadNewChanges(directory="photos/local-impact-photos/",smartUpload=False)
-    # uploadNewChanges(directory="results/stripes/",smartUpload=False)
-    uploadFile("Site Files/map-page/countries2.js", uploadFilePath="map-stuff.js")
+    #uploadNewChanges(directory="results/stripes-svg/",smartUpload=False)
+    #uploadFile("Site Files/map-page/countries2.js", uploadFilePath="map-stuff.js")
     #uploadFile("Map Stuff/mapData.csv",uploadFilePath="mapData2.csv")
     #uploadFile("test5.svg",uploadFilePath="test5.svg")
+    uploadFile("results/json/US.json",uploadFilePath="v3/json/US.json")
+    uploadAll("results/stripes/US.png")
     pass
+
+# method to upload all of the image/file types to s3 by inputting the stripes file
+def uploadAll(file,chartTypes=["label","labeled-bars","labeled-stripes","snap-sticker","stripes","twitter-card","stripes-svg"]):
+    for chartType in chartTypes:
+        if not "svg" in chartType:
+            uploadFile(file.replace("/stripes/","/"+chartType+"/"),uploadFilePath="v3/"+file.replace("/stripes/","/"+chartType+"/").replace("results/",""))
+        else:
+            uploadFile("results/"+chartType+".svg",uploadFilePath=chartType+".svg")
 
 # this is the main function that will be called when the script is run
 # if the file is imported, it will not run the test function
