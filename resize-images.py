914f06e417b514d9a6c84f6f0e4c6e66994759fa
Nikhil <60348432+nikhilrado@users.noreply.github.com>
2022-04-11 23:59:36 -0400

    added machine learning image cropping CU-271thrm

--- resize-images.py
+++ resize-images.py
@@ -2,7 +2,7 @@ import os
 from PIL import Image
 import globals
 
-def cropAndResizeSenatorImages(filepath,savepath):
+def cropAndResizeSenatorImages(filepath,savepath,crop_dimensions=False):
     # Opens a image in RGB mode
     im = Image.open(filepath)
     ogSizeKB = os.path.getsize(filepath)/1000
@@ -12,7 +12,10 @@ def cropAndResizeSenatorImages(filepath,savepath):
     
     #resizes the image to be square positioned at the top, and crops it
     #mimics the original css croping, so we don't have to have the cropped part in the image file
-    left,top,right,bottom = 0,0,width,width
+    if crop_dimensions:
+        left,top,right,bottom = crop_dimensions
+    else:
+        left,top,right,bottom = 0,0,width,width
 
     im1 = im.crop((left, top, right, bottom))
     im1 = im1.resize((200, 200))
@@ -30,6 +33,65 @@ def cropAndResizeSenatorImages(filepath,savepath):
     #im1.show()
 
 
+import boto3
+import base64
+from es_secrets import s3
+
+def detect_faces(photo):
+
+    client=boto3.client('rekognition', region_name='us-east-2', aws_access_key_id=s3.access_key, aws_secret_access_key=s3.secret_access_key)
+
+    with open(photo, 'rb') as image:
+        base64_image=base64.b64encode(image.read())
+        base_64_binary = base64.decodebytes(base64_image)
+
+        response = client.detect_faces(Image={'Bytes': base_64_binary}, Attributes=['ALL'])
+
+    #print(response)
+
+    return response['FaceDetails']
+
+
+if __name__ == "__main__":
+    #main()
+    pass
+
+def crop_to_center(photo,savePath,border=.12):
+    im = Image.open(photo)
+    data = detect_faces(photo)[0]['BoundingBox']
+    width, height = im.size
+
+    print("yeet")
+
+    left = (data['Left'] - border) * width
+    print(left)
+    top = (data['Top'] - border) * height
+    print(top)
+    right = (data['Left'] + data['Width'] + border) * width
+    print(right)
+    bottom = (data['Top'] + data['Height'] + border) * height
+
+
+    box_height = bottom - top
+    box_width = right - left
+
+    if box_height > box_width:
+        left = left - (box_height - box_width) / 2
+        right = right + (box_height - box_width) / 2
+    else:
+        top = top - (box_width - box_height) / 2
+        bottom = bottom + (box_width - box_height) / 2
+    
+    cropAndResizeSenatorImages(photo,savePath,crop_dimensions=(left,top,right,bottom))
+
+
 for photo in globals.getAllFilesInDir("photos/senators"):
     savePath = photo.replace("/","/compressed/")
-    cropAndResizeSenatorImages(photo,savePath)
+    #cropAndResizeSenatorImages(photo,savePath)
+    errors = []
+    try:
+        crop_to_center(photo,savePath)
+    except:
+        print("Error cropping: %s" % photo)
+        errors.append(photo)
+print(errors)
