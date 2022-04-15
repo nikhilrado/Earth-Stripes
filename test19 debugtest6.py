e566058ad2716cdde422d643938d8aa3c2b7a488
Nikhil <60348432+nikhilrado@users.noreply.github.com>
2022-04-15 15:36:39 -0400

    Delete test19 debugtest6.py
    file not needed

--- test19 debugtest6.py
+++
@@ -1,6 +0,0 @@
-import test6
-
-fileName = "G:\.shortcut-targets-by-id/1-78WtuBsUrKVKWF1NKxPcsrf1nvacux2/AP CSP VS Code Workspace/World Data Stuff/data/processed/CU - AnnTemp 1901-2020.csv"
-imagePath = 'G:\.shortcut-targets-by-id/1-78WtuBsUrKVKWF1NKxPcsrf1nvacux2/AP CSP VS Code Workspace/cubatest2'
-
-test6.createChart(fileName,imagePath)
