28448de8e9e6fa8899c2898ae8c113831cedb072
Nikhil <60348432+nikhilrado@users.noreply.github.com>
2023-02-01 16:29:09 -0500

    updated formatting and moved files (CU-867797247)

--- data-connectors/energy-data-to-json.py
+++ data-connectors/energy-data-to-json.py
@@ -1,92 +1,210 @@
-import csv
+import csv, sys
+sys.path.insert(0, './')
 import globals
 import manageJSON
 
-# opens the file from the eia website
-# go to link then select options, then select consumption and production for all countries
-# https://www.eia.gov/international/data/world/total-energy/total-energy-consumption
-f = open("../Earth Stripes Codebase/data/eia-energy-consumption-production-data.csv", encoding='utf-8-sig')
+# # opens the file from the eia website
+# # go to link then select options, then select consumption and production for all countries
+# # https://www.eia.gov/international/data/world/total-energy/total-energy-consumption
+# f = open("../Earth Stripes Codebase/data/eia-energy-consumption-production-data.csv", encoding='utf-8-sig')
 
-# opens file and puts data into list
-csv_f = csv.reader(f)
+# # opens file and puts data into list
+# csv_f = csv.reader(f)
 
-all_energy_data = []
-for row in csv_f:
-    all_energy_data.append(row)
+# all_energy_data = []
+# for row in csv_f:
+#     all_energy_data.append(row)
 
-# sets the header of the data which is the same for all days (then converts all to int)
-years = list(map(int, all_energy_data[1][2:-1]))
+# # sets the header of the data which is the same for all days (then converts all to int)
+# years = list(map(int, all_energy_data[1][2:-1]))
 
-count = 0
-failed = 0
-list_of_failed = []
+# count = 0
+# failed = 0
+# list_of_failed = []
 
-def country_to_json(list):
-    global count
+# def country_to_json(list):
+#     global count
 
-    country = list[0][1]
-    total_consumption = list[8][2:-1]
-    container = {
-        "energy consumption": []
-    }
+#     country = list[0][1]
+#     total_consumption = list[8][2:-1]
+#     container = {
+#         "energy consumption": []
+#     }
 
-    # calculates the percent by dividing the data by the whole
-    def get_percent(data_list):
-        for i in range(len(data_list)):
-            # if there is an error (no data, or "--" in column), null will be placed in the json, which allows the chart to render
-            try:
-                data_list[i] = round(float(data_list[i]) /
-                                    float(total_consumption[i])*100, 1)
-            except:
-                data_list[i] = None
+#     # calculates the percent by dividing the data by the whole
+#     def get_percent(data_list):
+#         for i in range(len(data_list)):
+#             # if there is an error (no data, or "--" in column), null will be placed in the json, which allows the chart to render
+#             try:
+#                 data_list[i] = round(float(data_list[i]) /
+#                                     float(total_consumption[i])*100, 1)
+#             except:
+#                 data_list[i] = None
 
-        return(data_list)
+#         return(data_list)
 
-    # this is the template for the json
-    z = {
-        "years": years,
-        "total consumption": total_consumption,
-        "coal": get_percent(list[9][2:-1]),
-        "natural gas": get_percent(list[10][2:-1]),
-        "petroleum and other liquids": get_percent(list[11][2:-1]),
-        "nuclear": get_percent(list[13][2:-1]),
-        "renewables and others": get_percent(list[14][2:-1]),
-    }
+#     # this is the template for the json
+#     z = {
+#         "years": years,
+#         "total consumption": total_consumption,
+#         "coal": get_percent(list[9][2:-1]),
+#         "natural gas": get_percent(list[10][2:-1]),
+#         "petroleum and other liquids": get_percent(list[11][2:-1]),
+#         "nuclear": get_percent(list[13][2:-1]),
+#         "renewables and others": get_percent(list[14][2:-1]),
+#     }
 
-    # adds the data to its container then prints it, along with the country
-    container["energy consumption"].append(z)
-    count += 1
-    print(country + " " + str(count))
-    #print(json.dumps(container, indent=2))
+#     # adds the data to its container then prints it, along with the country
+#     container["energy consumption"].append(z)
+#     count += 1
+#     print(country + " " + str(count))
+#     #print(json.dumps(container, indent=2))
 
-    # gets the country's country code from the globals file, then adds the data to the country's json file
-    try:
-        countryCode = globals.getCountryCode(country)
-    except:
-        three_letter_country_code = list[1][0].split("-")[2]
-        print(three_letter_country_code)
-        countryCode = globals.getISOconverted(three_letter_country_code)
-    manageJSON.updateDataObjet("results/json/"+countryCode+".json", "energy consumption", z)
-    # manageJSON.updateDataObjet("JSON stuff/AZtest.json","energy consumption",z) #use for testing
+#     # gets the country's country code from the globals file, then adds the data to the country's json file
+#     try:
+#         countryCode = globals.getCountryCode(country)
+#     except:
+#         three_letter_country_code = list[1][0].split("-")[2]
+#         print(three_letter_country_code)
+#         countryCode = globals.getISOconverted(three_letter_country_code)
+#     manageJSON.updateDataObjet("results/json/"+countryCode+".json", "energy consumption", z)
+#     # manageJSON.updateDataObjet("JSON stuff/AZtest.json","energy consumption",z) #use for testing
 
 
-# chops off last two years because data wasn't reliable
-rows_list_data = all_energy_data[2:]
-for k in range(len(rows_list_data)):
-    row = rows_list_data[k]
+# # chops off last two years because data wasn't reliable
+# rows_list_data = all_energy_data[2:]
+# for k in range(len(rows_list_data)):
+#     row = rows_list_data[k]
 
-    data_for_one_country_list = []
-    # just checks to make sure data is in increments of 15
-    if row[0] == "":
-        for j in range(15):
-            data_for_one_country_list.append(rows_list_data[k+j])
+#     data_for_one_country_list = []
+#     # just checks to make sure data is in increments of 15
+#     if row[0] == "":
+#         for j in range(15):
+#             data_for_one_country_list.append(rows_list_data[k+j])
+#         try:
+#             country_to_json(data_for_one_country_list)
+#         except:
+#             failed += 1
+#             list_of_failed.append(data_for_one_country_list[0][1])
+#             pass
+
+# print("success: " + str(count-failed)+"/"+str(count))
+# print("failed: " + str(failed)+"/"+str(count))
+# print(list_of_failed)
+
+class country_electricity_consumption:
+    # opens the file from the eia website
+    # go to link then select options, then select consumption and production for all countries
+    # https://www.eia.gov/international/data/world/total-energy/total-energy-consumption
+    f = open("../Earth Stripes Codebase/data/eia/eia-electricity-consumption-2022.csv", encoding='utf-8-sig')
+
+    # opens file and puts data into list
+    csv_f = csv.reader(f)
+
+    all_energy_data = []
+    for row in csv_f:
+        all_energy_data.append(row)
+
+    # sets the header of the data which is the same for all days (then converts all to int)
+    years = list(map(int, all_energy_data[1][2:]))
+
+    count = 0
+    failed = 0
+    list_of_failed = []
+
+    def csv_to_json(csv):
+        temp = {}
+        for row in csv:
+            temp[row[1]] = row[2]
+        return temp
+
+
+
+    def country_json_to_json(data):
+        # calculates the percent by dividing the data by the whole
+        def get_percent(data_list, total_consumption_list):
+            for i in range(len(data_list)):
+                # if there is an error (no data, or "--" in column), null will be placed in the json, which allows the chart to render
+                try:
+                    data_list[i] = round(float(data_list[i]) /
+                                        float(total_consumption_list[i])*100, 1)
+                except:
+                    data_list[i] = None
+
+            return(data_list)
+
+        global count
+        container = {
+            "energy consumption": []
+        }
+
+        # this is the template for the json
+        z = {
+            "years": country_electricity_consumption.years,
+            "total consumption": data["    Generation (billion kWh)"],
+            "nuclear": get_percent(data["Nuclear"], data["    Generation (billion kWh)"]),
+            "coal": get_percent(data),
+            "natural gas": get_percent(data[10][2:-1]),
+            "petroleum and other liquids": get_percent(data[11][2:-1]),
+            "nuclear": get_percent(data[13][2:-1]),
+            "renewables and others": get_percent(data[14][2:-1]),
+        }
+
+    def country_to_json(list):
+
+        country = list[0][1]
+        total_consumption = list[8][2:-1]
+        container = {
+            "energy consumption": []
+        }
+
+
+
+        # this is the template for the json
+        z = {
+            "years": years,
+            "total consumption": total_consumption,
+            "coal": get_percent(list[9][2:-1]),
+            "natural gas": get_percent(list[10][2:-1]),
+            "petroleum and other liquids": get_percent(list[11][2:-1]),
+            "nuclear": get_percent(list[13][2:-1]),
+            "renewables and others": get_percent(list[14][2:-1]),
+        }
+
+        # adds the data to its container then prints it, along with the country
+        container["energy consumption"].append(z)
+        count += 1
+        print(country + " " + str(count))
+        #print(json.dumps(container, indent=2))
+
+        # gets the country's country code from the globals file, then adds the data to the country's json file
         try:
-            country_to_json(data_for_one_country_list)
+            countryCode = globals.getCountryCode(country)
         except:
-            failed += 1
-            list_of_failed.append(data_for_one_country_list[0][1])
-            pass
+            three_letter_country_code = list[1][0].split("-")[2]
+            print(three_letter_country_code)
+            countryCode = globals.getISOconverted(three_letter_country_code)
+        # manageJSON.updateDataObjet("results/json/"+countryCode+".json", "energy consumption", z)
+        # manageJSON.updateDataObjet("JSON stuff/AZtest.json","energy consumption",z) #use for testing
 
-print("success: " + str(count-failed)+"/"+str(count))
-print("failed: " + str(failed)+"/"+str(count))
-print(list_of_failed)
+
+    # chops off last two years because data wasn't reliable
+    rows_list_data = all_energy_data #[2:]
+    for k in range(len(rows_list_data)):
+        row = rows_list_data[k]
+
+        data_for_one_country_list = []
+        # just checks to make sure data is in increments of 15
+        if row[0] == "":
+            for j in range(15):
+                data_for_one_country_list.append(rows_list_data[k+j])
+            try:
+                print(csv_to_json(data_for_one_country_list))
+            except:
+                failed += 1
+                list_of_failed.append(data_for_one_country_list[0][1])
+                pass
+
+    print("success: " + str(count-failed)+"/"+str(count))
+    print("failed: " + str(failed)+"/"+str(count))
+    print(list_of_failed)
--- data-connectors/yale-climate-data-to-JSON.py
+++ data-connectors/yale-climate-data-to-JSON.py
@@ -1,16 +1,21 @@
 import csv
 import json
 
-#This program takes the data from the Yale Climate Opinion Survey and places it in the respective JSON files
-#Note: changed the name of DC state from "District of Columbia" to "Washington D.C." then deleted the DC county line in the data file
+# This program takes the data from the Yale Climate Opinion Survey and places it in the respective JSON files
+# Note: changed the name of DC state from "District of Columbia" to "Washington D.C." then deleted the DC county line in the data file
 
-#convert between long or short name of the state
-stateFullName = ["County","Washington D.C.","Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"]
-stateShortName = ["ty","DC","AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"]
+# convert between long or short name of the state
+stateFullName = ["County", "Washington D.C.", "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri",
+                 "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
+stateShortName = ["ty", "DC", "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN",
+                  "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+
+
 def getStateAbrev(state):
     if len(state) == 2:
         return stateFullName[stateShortName.index(state)]
     return stateShortName[stateFullName.index(state)]
+
 
 f = open('data/yale-climate/YCOM_2020_Data.csv', "r")
 csv_f = csv.reader(f)
@@ -19,10 +24,10 @@ rowsList = []
 for row in csv_f:
     rowsList.append(row)
 
-dataHeader = rowsList[0] #sets first row as headers
-rowsList.pop(0) #removes first row
+dataHeader = rowsList[0]  # sets first row as headers
+rowsList.pop(0)  # removes first row
 
-#gets the resource ID of the file by seeing what location category the yale data belongs to
+# gets the resource ID of the file by seeing what location category the yale data belongs to
 def getYaleResourceID(row):
     resourceID = None
     if row[0] == "County":
@@ -36,8 +41,9 @@ def getYaleResourceID(row):
         resourceID = row[2]
     return resourceID
 
+
 for row in rowsList:
-    #print(row)
+    # print(row)
     resourceID = getYaleResourceID(row)
     try:
         f = open('results/json/'+resourceID+'.json')
@@ -46,25 +52,25 @@ for row in rowsList:
     f = f.read()
     f = json.loads(f)
 
-    #template JSON/python dict
+    # template JSON/python dict
     y = {
-        "YaleClimateOpinionData2020":{
-            "metadata":{
-                "source":'Howe, Peter D., Matto Mildenberger, Jennifer R. Marlon, and Anthony Leiserowitz (2015). “Geographic variation in opinions on climate change at state and local scales in the USA.” Nature Climate Change, doi:10.1038/nclimate2583',
-                "disclaimer":"The YPCCC bears no responsibility for the analyses or interpretations of the data presented here."
+        "YaleClimateOpinionData2020": {
+            "metadata": {
+                "source": 'Howe, Peter D., Matto Mildenberger, Jennifer R. Marlon, and Anthony Leiserowitz (2015). “Geographic variation in opinions on climate change at state and local scales in the USA.” Nature Climate Change, doi:10.1038/nclimate2583',
+                "disclaimer": "The YPCCC bears no responsibility for the analyses or interpretations of the data presented here."
             },
-            "data":{}
+            "data": {}
         }
     }
-    
+
     print(y)
-    if len(dataHeader) != len(row): #just a check
+    if len(dataHeader) != len(row):  # just a check
         print("---------Error: potentially missing data")
     for i in range(len(dataHeader)):
-        y["YaleClimateOpinionData2020"]["data"][dataHeader[i]]=row[i]
+        y["YaleClimateOpinionData2020"]["data"][dataHeader[i]] = row[i]
     f.update(y)
     print(y)
-    
-    #sets name and saves file
+
+    # sets name and saves file
     with open('results/json/'+resourceID+'.json', "w") as myfile:
         myfile.write(json.dumps(f, indent=2))
